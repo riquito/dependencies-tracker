7ba328e7d62167fcbae13160a765da1bc923b6d9
Riccardo Attilio Galli <riccardo@sideralis.org>
2024-03-29 12:08:05 -0700

    Refactor yarn-why api into its own file

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import { WASI, File, Fd, OpenFile, ConsoleStdout, PreopenDirectory, wasi } from "@bjorn3/browser_wasi_shim";
+import { YarnWhyJSONOutput, yarnWhy } from './yarn-why'
 
 // Icon from https://lucide.dev/icons/microscope
 import searchIcon from '/search.svg'
@@ -25,13 +25,6 @@ function SearchInput({ onChange }: SearchInputProps) {
     />
   )
 }
-
-type YarnWhyJSONOutputLeaf = {
-  descriptor: string,
-  children: YarnWhyJSONOutputLeaf[]
-}
-
-type YarnWhyJSONOutput = YarnWhyJSONOutputLeaf[]
 
 type LockFileProps = {
   repo: string;
@@ -78,140 +71,6 @@ function getLockfilesWithMaybePackage(lockFiles: LockfilesMap, packageName: stri
   })
 }
 
-function escapeRegExp(t: string): string {
-  return t.replace(/[.*+?^${}()|[\]\\]/g, "\\$&"); // $& means the whole matched string
-}
-
-async function yarnWhy_example() {
-  let args = ["bin", "-h"];
-  let env = ["FOO=bar"];
-  let fds = [
-    new OpenFile(new File([])), // stdin
-    ConsoleStdout.lineBuffered(msg => console.log(`[WASI stdout] ${msg}`)),
-    ConsoleStdout.lineBuffered(msg => console.warn(`[WASI stderr] ${msg}`)),
-    new PreopenDirectory(".", {
-      "example.c": new File(new TextEncoder().encode(`#include "a"`)),
-      "hello.rs": new File(new TextEncoder().encode(`fn main() { println!("Hello World!"); }`)),
-    }),
-  ];
-  let wasi = new WASI(args, env, fds);
-
-  //let wasm = await WebAssembly.compileStreaming(fetch('/yarnWhy.wasm'));
-  //console.log('mmm', yarnWhyData.slice(0, 10))
-  let wasm = await WebAssembly.compile(fromHexString(yarnWhyData));
-  let inst = await WebAssembly.instantiate(wasm, {
-    "wasi_snapshot_preview1": wasi.wasiImport,
-  });
-  wasi.start(inst as any);
-
-}
-
-// Keep storing data until the file descriptor is closed,
-// then write it all at once.
-class ConsoleExhaust extends Fd {
-  write: (buffer: Uint8Array) => void;
-  buffer: Uint8Array[];
-
-  constructor(write: (buffer: Uint8Array) => void) {
-    super();
-    this.write = write;
-    this.buffer = [];
-  }
-
-  fd_filestat_get(): { ret: number; filestat: wasi.Filestat } {
-    const filestat = new wasi.Filestat(
-      wasi.FILETYPE_CHARACTER_DEVICE,
-      BigInt(0),
-    );
-    return { ret: 0, filestat };
-  }
-
-  fd_fdstat_get(): { ret: number; fdstat: wasi.Fdstat | null } {
-    const fdstat = new wasi.Fdstat(wasi.FILETYPE_CHARACTER_DEVICE, 0);
-    fdstat.fs_rights_base = BigInt(wasi.RIGHTS_FD_WRITE);
-    return { ret: 0, fdstat };
-  }
-
-  fd_write(
-    view8: Uint8Array,
-    iovs: Array<wasi.Ciovec>,
-  ): { ret: number; nwritten: number } {
-    let nwritten = 0;
-    for (const iovec of iovs) {
-      const buffer = view8.slice(iovec.buf, iovec.buf + iovec.buf_len);
-      //this.write(buffer);
-      this.buffer.push(buffer);
-      nwritten += iovec.buf_len;
-    }
-    return { ret: 0, nwritten };
-  }
-  fd_close(): number {
-    // Get the total length of all arrays.
-    const length = this.buffer.reduce((a, b) => a + b.length, 0);
-
-    // Create a new array with total length and merge all source arrays.
-    let mergedArray = new Uint8Array(length);
-    let offset = 0;
-    this.buffer.forEach(item => {
-      mergedArray.set(item, offset);
-      offset += item.length;
-    });
-
-    //this.write(new TextEncoder().encode(this.buffer.map(b => new TextDecoder().decode(b, { stream: true })).join('')));
-    this.write(mergedArray);
-
-    return 0;
-  }
-}
-
-
-async function yarnWhy({ lockFile, query, wasm }: { lockFile: string, query: string, wasm: WebAssembly.Module }): Promise<string> {
-
-  let output = ''
-  let err_output = ''
-
-  let args = ["yarn-why", '--json', query];
-  let env = ["FOO=bar"];
-  let fds = [
-    new OpenFile(new File(new TextEncoder().encode(lockFile))), // stdin
-    new ConsoleExhaust(buffer => {
-      const stdout = new TextDecoder("utf-8", { fatal: false }).decode(buffer);
-      output = stdout;
-    }),
-    new ConsoleExhaust(buffer => {
-      const stderr = new TextDecoder("utf-8", { fatal: false }).decode(buffer)
-      err_output = stderr;
-    }),
-    new PreopenDirectory(".", {
-      "yarn.lock": new File(new TextEncoder().encode(lockFile)),
-      "hello.rs": new File(new TextEncoder().encode(`fn main() { println!("Hello World!"); }`)),
-    }),
-  ];
-  let wasi = new WASI(args, env, fds, { debug: true });
-
-  let inst = await WebAssembly.instantiate(wasm, {
-    "wasi_snapshot_preview1": wasi.wasiImport,
-  });
-  const exit_code = wasi.start(inst as any);
-  // File descriptors are not closed automatically when the program exits,
-  // so we need to close them manually (see FIXME v0.3 in @bjorn3/browser_wasi_shim)
-  fds.forEach(fd => fd.fd_close());
-
-  // output now is not empty because the file descriptor is closed
-  // and it's handler was called synchronously
-
-  if (exit_code !== 0) {
-    if (output === 'Package not found\n') {
-      return ''
-    }
-
-    throw new Error(err_output);
-  }
-
-  return output
-}
-
-
 function App() {
   const [packageQuery, setPackageQuery] = useState('');
   const [reposWithMaybePackage, setReposWithMaybePackage] = useState<string[]>([]);
@@ -226,15 +85,12 @@ function App() {
 
   useEffect(() => {
     if (packageQuery && wasm && reposWithMaybePackage.length > 0) {
-      Promise.all(reposWithMaybePackage.map<Promise<[string, string]>>((repo =>
+      Promise.all(reposWithMaybePackage.map<Promise<[string, YarnWhyJSONOutput | null]>>((repo =>
         yarnWhy({ lockFile: lockFiles[repo], query: packageQuery, wasm }).then((output) => {
           return [repo, output]
         })
-      ))).then((pairs: [string, string][]) => {
-        const pairsRepoMatches = pairs.filter(([_, output]) => output !== '').map<[string, YarnWhyJSONOutput]>(([repo, output]) => {
-          return [repo, JSON.parse(output) as YarnWhyJSONOutput]
-        })
-
+      ))).then((pairs: [string, YarnWhyJSONOutput | null][]) => {
+        const pairsRepoMatches = pairs.filter(([_, output]) => output !== null) as [string, YarnWhyJSONOutput][];
         setFoundPackages(pairsRepoMatches);
       })
     }
@@ -258,7 +114,6 @@ function App() {
     </>
   )
 }
-
 
 
 export default App
---
+++ src/yarn-why.ts
@@ -0,0 +1,107 @@
+import { WASI, File, Fd, OpenFile, wasi } from "@bjorn3/browser_wasi_shim";
+
+// Keep storing data until the file descriptor is closed,
+// then write it all at once.
+class ConsoleExhaust extends Fd {
+    write: (buffer: Uint8Array) => void;
+    buffer: Uint8Array[];
+
+    constructor(write: (buffer: Uint8Array) => void) {
+        super();
+        this.write = write;
+        this.buffer = [];
+    }
+
+    fd_filestat_get(): { ret: number; filestat: wasi.Filestat } {
+        const filestat = new wasi.Filestat(
+            wasi.FILETYPE_CHARACTER_DEVICE,
+            BigInt(0),
+        );
+        return { ret: 0, filestat };
+    }
+
+    fd_fdstat_get(): { ret: number; fdstat: wasi.Fdstat | null } {
+        const fdstat = new wasi.Fdstat(wasi.FILETYPE_CHARACTER_DEVICE, 0);
+        fdstat.fs_rights_base = BigInt(wasi.RIGHTS_FD_WRITE);
+        return { ret: 0, fdstat };
+    }
+
+    fd_write(
+        view8: Uint8Array,
+        iovs: Array<wasi.Ciovec>,
+    ): { ret: number; nwritten: number } {
+        let nwritten = 0;
+        for (const iovec of iovs) {
+            const buffer = view8.slice(iovec.buf, iovec.buf + iovec.buf_len);
+            //this.write(buffer);
+            this.buffer.push(buffer);
+            nwritten += iovec.buf_len;
+        }
+        return { ret: 0, nwritten };
+    }
+    fd_close(): number {
+        // Get the total length of all arrays.
+        const length = this.buffer.reduce((a, b) => a + b.length, 0);
+
+        // Create a new array with total length and merge all source arrays.
+        const mergedArray = new Uint8Array(length);
+        let offset = 0;
+        this.buffer.forEach(item => {
+            mergedArray.set(item, offset);
+            offset += item.length;
+        });
+
+        this.write(mergedArray);
+
+        return 0;
+    }
+}
+
+
+type YarnWhyJSONOutputLeaf = {
+    descriptor: string,
+    children: YarnWhyJSONOutputLeaf[]
+}
+
+export type YarnWhyJSONOutput = YarnWhyJSONOutputLeaf[]
+
+export async function yarnWhy({ lockFile, query, wasm }: { lockFile: string, query: string, wasm: WebAssembly.Module }): Promise<YarnWhyJSONOutput | null> {
+    let output = ''
+    let err_output = ''
+
+    const args = ["yarn-why", '--json', query];
+    const env: string[] = [];
+    const fds = [
+        new OpenFile(new File(new TextEncoder().encode(lockFile))), // stdin
+        new ConsoleExhaust(buffer => {
+            const stdout = new TextDecoder("utf-8", { fatal: false }).decode(buffer);
+            output = stdout;
+        }),
+        new ConsoleExhaust(buffer => {
+            const stderr = new TextDecoder("utf-8", { fatal: false }).decode(buffer)
+            err_output = stderr;
+        }),
+    ];
+    const wasi = new WASI(args, env, fds, { debug: true });
+
+    const inst = await WebAssembly.instantiate(wasm, {
+        "wasi_snapshot_preview1": wasi.wasiImport,
+    });
+    const exit_code = wasi.start(inst as any);
+    // File descriptors are not closed automatically when the program exits,
+    // so we need to close them manually (see FIXME v0.3 in @bjorn3/browser_wasi_shim)
+    fds.forEach(fd => fd.fd_close());
+
+    // output now is not empty because the file descriptor is closed
+    // and it's handler was called synchronously
+
+    if (exit_code !== 0) {
+        if (output === 'Package not found\n') {
+            return null;
+        }
+
+        throw new Error(err_output);
+    }
+
+    return JSON.parse(output) as YarnWhyJSONOutput
+}
